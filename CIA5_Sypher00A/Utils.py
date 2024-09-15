38ea8115c4457dacdf5480ab926eb9e165b4cf1c
lalit <lalitg@iitbhilai.ac.in>
2024-09-15 11:50:18 +0530

    Done

--- CIA5_Sypher00A/Utils.py
+++ CIA5_Sypher00A/Utils.py
@@ -1,14 +1,14 @@
 import subprocess
 
-sbox = [15, 14, 11, 12, 6, 13, 7, 8, 0, 3, 9, 10, 4, 2, 1, 5]
+sbox = [15, 0, 9, 1, 12, 3, 25, 22, 4, 29, 7, 27, 16, 21, 26, 14,31, 24, 2, 5, 28, 30, 10, 6, 17, 20, 19, 11, 18, 23, 13, 8]
 
 # return the message and cipher text pair
 # by multiple query to oracle
 def getPairs(executable):
     message = []
     cipherText = []
-    for i in range(16):
-        result = subprocess.run([executable], input=str(i),
+    for i in range(32):
+        result = subprocess.run([executable, str(i)], 
                                 text=True, capture_output=True)
         lines = result.stdout.strip().split('\n')
         if len(lines) == 2:
@@ -41,16 +41,17 @@ def getXor(num1, num2):
 
 # Creating LAT 
 def createLat():
-    ans = [[0 for _ in range(16)]for _ in range(16)]
-    for x in range(1, 16):
-        for y in range(1, 16):
+    ans = [[0 for _ in range(32)]for _ in range(32)]
+    for x in range(1, 32):
+        for y in range(1, 32):
             count = 0
-            for z in range(16):
+            for z in range(32):
                 alpha_x = getXor(x, z)
                 beta_x = getXor(y, sbox[z])
                 if alpha_x == beta_x:
                     count += 1
-            ans[x][y] = count - 8
+            ans[x][y] = count - 16
+    
     return ans 
 
 
@@ -60,10 +61,11 @@ def getMaskPairs():
     LAT = createLat()
     alpha = []
     beta = []
-    for i in range(1, 16):
-        for j in range(1, 16):
-            if LAT[i][j] != 0 and abs(LAT[i][j]) != 2:
+    for i in range(1, 32):
+        for j in range(1, 32):
+            if LAT[i][j] != 0 and abs(LAT[i][j]) >= 4:
                 alpha.append(i)
                 beta.append(j)
-
+    for i in LAT:
+        print(i)
     return alpha, beta, LAT 
