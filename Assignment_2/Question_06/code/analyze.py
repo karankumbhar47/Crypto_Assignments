a1cbe43c9b6dc283c93e1116861996b5f52bd049
Karan Kumbhar <111757554+karankumbhar47@users.noreply.github.com>
2024-09-22 21:27:55 +0530

    submitted assignment

--- Assignment_2/Question_06/code/analyze.py
+++ Assignment_2/Question_06/code/analyze.py
@@ -4,31 +4,36 @@ from utils import generate_ddt, apply_permutation, bit16_to_tuple, tuple_to_16bi
 
 S_Box = [6, 4, 12, 5, 0, 7, 2, 14, 1, 15, 3, 13, 8, 10, 9, 11]
 lookupTable = generate_ddt(sbox=S_Box)
-threshold = 6 
+threshold = 6
+
 
 def is_valid_transition(x, y):
     return lookupTable[x][y] >= threshold
+
 
 def calculate_transition_probability(node1, node2):
     pr_x_to_y = lookupTable[node1[0]][node2[0]]/16
     pr_x_to_w = lookupTable[node1[1]][node2[1]]/16
     pr_x_to_z = lookupTable[node1[2]][node2[2]]/16
     pr_x_to_k = lookupTable[node1[3]][node2[3]]/16
-    pr =  pr_x_to_y * pr_x_to_w * pr_x_to_z * pr_x_to_k
+    pr = pr_x_to_y * pr_x_to_w * pr_x_to_z * pr_x_to_k
     return pr
+
 
 def active_count(path):
     total = 0
     boolVar = False
-    for i,node in enumerate(path):
+    for i, node in enumerate(path):
         curr = sum(1 for val in node if val != 0)
-        if(i==len(path)-1 and curr==2): boolVar = True
-        total+= curr
-    return [total,boolVar]
+        if (i == len(path)-1 and curr == 2):
+            boolVar = True
+        total += curr
+    return [total, boolVar]
+
 
 def dfs(current_node, depth, path, total_probability, paths):
     if depth == 4:
-        if(active_count(path)[1]):
+        if (active_count(path)[1]):
             paths.append((path[:], total_probability, active_count(path)[0]))
         return
 
@@ -38,7 +43,8 @@ def dfs(current_node, depth, path, total_probability, paths):
            is_valid_transition(current_node[2], next_node[2]) and \
            is_valid_transition(current_node[3], next_node[3]):
 
-            transition_prob = calculate_transition_probability(current_node, next_node)
+            transition_prob = calculate_transition_probability(
+                current_node, next_node)
             new_total_probability = total_probability * transition_prob
 
             permuted_bits = apply_permutation(tuple_to_16bit(next_node))
@@ -46,6 +52,7 @@ def dfs(current_node, depth, path, total_probability, paths):
             path.append(next_node)
             dfs(next_node, depth + 1, path, new_total_probability, paths)
             path.pop()  # Backtrack
+
 
 def generate_next_nodes(node):
     next_nodes = []
@@ -57,10 +64,12 @@ def generate_next_nodes(node):
                     next_nodes.append(next_node)
     return next_nodes
 
+
 def find_all_paths(source_node):
     paths = []
     dfs(source_node, 0, [source_node], 1, paths)
     return paths
+
 
 def main():
     maxProb = 0
@@ -75,13 +84,16 @@ def main():
 
                     # Print the paths and their probabilities
                     for path, probability, active_number in all_paths:
-                        if(maxProb<probability):
+                        if (maxProb < probability):
                             maxProb = probability
                             maxPath = path
                             box = active_number
-                        print(f"Path: {path}, Probability: {probability}, Active Number: {active_number}")
+                        print(
+                            f"Path: {path}, Probability: {probability}, Active Number: {active_number}")
 
-    print("\nmost optimized path is ",maxPath)
-    print("\nprob of maxPath ",maxProb)
+    print("\nmost optimized path is ", maxPath)
+    print("\nprob of maxPath ", maxProb)
+
 
 main()
+
