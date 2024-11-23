4f7dc1ec335ae335aef025e5a1e578909f063eaa
Karan Kumbhar <111757554+karankumbhar47@users.noreply.github.com>
2024-11-23 23:39:53 +0530

    added Midori implementation

---
+++ Midori(Term_Paper)/Midori/main.py
@@ -0,0 +1,10 @@
+from Midori import *
+
+key_128Bit = "687ded3b3c85b3f35b1009863e2a8cbf"
+WK, K0, K1 = KeyGen(key_128Bit)
+
+plainText = "42c20fd3b586879e"
+expected_output = "66bcdc6270d901cd"
+
+print("Output from Algorithm: ", *Midori64_Core(plainText, WK, K0, K1))
+print("Expected Output      : ", *stringToHexList(expected_output))
