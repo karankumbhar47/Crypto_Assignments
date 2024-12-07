ad1ebdcf8ab743e7c8591afd3f2ada954629d5aa
Karan Kumbhar <111757554+karankumbhar47@users.noreply.github.com>
2024-12-08 03:46:58 +0530

    software working

--- Midori(Term_Paper)/Software/karan.py
+++ Midori(Term_Paper)/Software/karan.py
@@ -1,47 +1,50 @@
 from Midori import *
 
 class ECBMode:
-    BLOCK_SIZE = 8 # Example block size in bytes
-
-    def __init__(self, encrypt_block, decrypt_block):
-        key_128Bit = "687ded3b3c85b3f35b1009863e2a8cbf"
-        self.WK, self.K0, self.K1 = KeyGen(key_128Bit)
+    def __init__(self, encrypt_block, decrypt_block, key):
+        self.key_128Bit = key 
+        self.WK, self.K0, self.K1 = KeyGen(self.key_128Bit)
         self.encrypt_block = encrypt_block
         self.decrypt_block = decrypt_block
+        self.BLOCK_SIZE = 16 
 
     def pad(self, data):
-        pad_length = self.BLOCK_SIZE - len(data) % self.BLOCK_SIZE
-        return data + bytes([pad_length] * pad_length)
+        byteData = bytes.fromhex(data)
+        pad_length = self.BLOCK_SIZE - len(byteData) % self.BLOCK_SIZE
+        return (byteData + bytes([pad_length] * pad_length)).hex()
 
     def unpad(self, data):
-        pad_length = data[-1]
-        return data[:-pad_length]
+        byteData = bytes.fromhex(data)
+        pad_length = byteData[-1]
+        return byteData[:-pad_length].hex()
 
     def encrypt_ecb(self, plaintext):
+        plaintext = ''.join(format(ord(char), '02x') for char in plaintext)
         plaintext = self.pad(plaintext)
         num_blocks = len(plaintext) // self.BLOCK_SIZE
         ciphertext = ''
 
         for i in range(num_blocks):
             block = plaintext[i * self.BLOCK_SIZE:(i + 1) * self.BLOCK_SIZE]
-            ciphertext += self.encrypt_block(block.hex(), self.WK, self.K0, self.K1)
+            ciphertext += self.encrypt_block(block, self.WK, self.K0, self.K1)
 
         return ciphertext
 
     def decrypt_ecb(self, ciphertext):
-        ciphertext = bytes.fromhex(ciphertext)
         num_blocks = len(ciphertext) // self.BLOCK_SIZE
         plaintext = ''
 
         for i in range(num_blocks):
             block = ciphertext[i * self.BLOCK_SIZE:(i + 1) * self.BLOCK_SIZE]
-            plaintext += self.decrypt_block(block.hex(), self.WK, self.K0, self.K1)
+            plaintext += self.decrypt_block(block, self.WK, self.K0, self.K1)
 
-        return self.unpad(bytes.fromhex(plaintext)).decode('utf-8')
+        plaintext = self.unpad(plaintext)
+        message = bytes.fromhex(plaintext).decode('utf-8')
+        return message
 
 
-ecb = ECBMode(Midori64_Core, Midori64_Core_Decrypt)
+ecb = ECBMode(Midori64_Core, Midori64_Core_Decrypt,"687ded3b3c85b3f35b1009863e2a8cbf")
 
-out = ecb.encrypt_ecb(b'karansunilkumbhar')
-out = ecb.decrypt_ecb(out)
-print(out)
+message = "message random message "
+out = ecb.decrypt_ecb(ecb.encrypt_ecb(message))
+print(out==message)
